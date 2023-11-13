2c52f086ea8f12375640091832f9967d201411f6
aalexebr <alex.ebr@icloud.com>
2023-11-13 15:13:44 +0100

    first

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,13 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+	
+	private int code;
+	private String name;
+	private String description;
+	private float iva;
+	
+	public Prodotto(int code, String name, String description) {
+		
+	}
+}

578b4ce55f63f03e45183449e465bfcb81fb1f2f
aalexebr <alex.ebr@icloud.com>
2023-11-13 15:33:42 +0100

    class progress

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -3,6 +3,10 @@ package org.lessons.java.shop;
 public class Main {
 	public static void main(String[] args) {
 		
+		
+		Prodotto prodotto1 = new Prodotto("ciao","description", 4.1);
+		
+		System.out.println(prodotto1);
 	}
 
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,13 +1,66 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 	
 	private int code;
 	private String name;
 	private String description;
-	private float iva;
+	private double iva;
 	
-	public Prodotto(int code, String name, String description) {
+	public Prodotto(String name, String description, double iva) {
+		setCode(code);
+		setName(name);
+		setDescription(description);
+		setIva(iva);
 		
 	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	private void setCode(int code) {
+		Random rnd = new Random();
+		this.code = rnd.nextInt(0,2147483647);
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	
+	public double getIva() {
+		return this.iva;
+	}
+	
+	@Override
+	
+	public String toString() {
+		return "Product:\n"
+				+ "code: " + getCode() + "\n"
+				+ "name: " + getName() + "\n"
+				+ "desc: " + getDescription()+ "\n"
+				+ "iva: " + getIva()+ "%\n";
+		
+	}
+	
+	
+	
 }
